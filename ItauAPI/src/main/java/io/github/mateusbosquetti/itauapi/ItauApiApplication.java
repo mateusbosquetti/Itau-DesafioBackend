b6aa798b2558e34119969d758e4c5c37bee35943
Mateus Henrique Bosquetti <mateushb123@gmail.com>
2025-03-08 13:35:28 -0300

    First Commit

---
+++ ItauAPI/src/main/java/io/github/mateusbosquetti/itauapi/ItauApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.mateusbosquetti.itauapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItauApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItauApiApplication.class, args);
+    }
+
+}
---
+++ ItauAPI/src/test/java/io/github/mateusbosquetti/itauapi/ItauApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mateusbosquetti.itauapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItauApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

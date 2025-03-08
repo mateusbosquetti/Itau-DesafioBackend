1662e8dcc4943a8add7f313a5d4f4afe1e9ec5bd
Mateus Henrique Bosquetti <mateushb123@gmail.com>
2025-03-08 15:39:31 -0300

    feat: Estatitisca implemented

--- ItauAPI/src/main/java/io/github/mateusbosquetti/itauapi/service/TransacaoService.java
+++ ItauAPI/src/main/java/io/github/mateusbosquetti/itauapi/service/TransacaoService.java
@@ -8,6 +8,12 @@ import io.github.mateusbosquetti.itauapi.repository.TransacaoRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @AllArgsConstructor
 public class TransacaoService {
@@ -24,8 +30,71 @@ public class TransacaoService {
         repository.deleteAll();
     }
 
+    public List<Transacao> ajustarFusoHorarioParaBrasil(List<Transacao> transacoes) {
+        ZoneOffset fusoBrasil = ZoneOffset.of("-03:00");
+
+        for (Transacao transacao : transacoes) {
+            OffsetDateTime dataHoraAtual = transacao.getDataHora();
+
+            OffsetDateTime dataHoraBrasil = dataHoraAtual.withOffsetSameInstant(fusoBrasil);
+
+            transacao.setDataHora(dataHoraBrasil);
+        }
+
+        return transacoes;
+    }
+
     public EstatisticaResponseDTO gerarEstatistica() {
-        return null;
+        OffsetDateTime horarioAtual = OffsetDateTime.now();
+        OffsetDateTime horario1minutosAtras = horarioAtual.minusSeconds(60);
+        List<Transacao> transacaoList = ajustarFusoHorarioParaBrasil(repository.findAll());
+        List<Transacao> transacoesValidas = new ArrayList<>();
+        for (Transacao transacao : transacaoList) {
+            if (
+                    transacao.getDataHora().getHour() == horario1minutosAtras.getHour() &&
+                            transacao.getDataHora().getMinute() > horario1minutosAtras.getMinute() ||
+                            transacao.getDataHora().getMinute() == horario1minutosAtras.getMinute() &&
+                                    transacao.getDataHora().getSecond() >= horario1minutosAtras.getSecond()
+            ) {
+                transacoesValidas.add(transacao);
+            }
+        }
+
+        if (transacoesValidas.isEmpty()) {
+            return new EstatisticaResponseDTO(
+                    0,
+                    BigDecimal.valueOf(0),
+                    BigDecimal.valueOf(0),
+                    BigDecimal.valueOf(0),
+                    BigDecimal.valueOf(0)
+            );
+        }
+
+        Integer count = transacoesValidas.size();
+        BigDecimal sum = BigDecimal.valueOf(0);
+        BigDecimal min = transacoesValidas.getFirst().getValor();
+        BigDecimal max = BigDecimal.valueOf(0);
+
+        for (Transacao transacao : transacoesValidas) {
+            sum = sum.add(transacao.getValor());
+
+            if (transacao.getValor().compareTo(max) > 0) {
+                max = transacao.getValor();
+            }
+            if (transacao.getValor().compareTo(min) < 0) {
+                min = transacao.getValor();
+            }
+        }
+
+        BigDecimal avg = sum.divide(BigDecimal.valueOf(transacoesValidas.size()));
+
+        return new EstatisticaResponseDTO(
+                count,
+                sum,
+                avg,
+                min,
+                max
+        );
     }
 
 }

3e3cf5421967affbbce5cba5babb950b284760f6
Mateus Henrique Bosquetti <mateushb123@gmail.com>
2025-03-08 15:38:39 -0300

    fix: Round valor

--- ItauAPI/src/main/java/io/github/mateusbosquetti/itauapi/model/dto/request/TransacaoRequestDTO.java
+++ ItauAPI/src/main/java/io/github/mateusbosquetti/itauapi/model/dto/request/TransacaoRequestDTO.java
@@ -5,13 +5,15 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 
 import java.math.BigDecimal;
+import java.time.OffsetDateTime;
 
 public record TransacaoRequestDTO(
         @NotNull @PositiveOrZero BigDecimal valor
 ) {
     public Transacao toEntity() {
         return Transacao.builder()
-                .valor(this.valor)
+                .valor(this.valor.setScale(2, BigDecimal.ROUND_HALF_UP))
+                .dataHora(OffsetDateTime.now())
                 .build();
     }
 }
